7f96d188b48824ffe2f5cb2aa0d2d72ad66651c4
RahJade21 <jaydregon@gmail.com>
2025-10-08 15:42:53 +0700

    Finalization for parking space tracker

--- ParkingSpacePicker.py
+++ ParkingSpacePicker.py
@@ -3,7 +3,12 @@ import pickle
 
 
 width, height = 107, 48
-posList = []
+
+try:
+  with open('CarParkPos', 'rb') as f:
+    posList = pickle.load(f)
+except:
+  posList = []
 
 def mouseClick(events, x, y, flags, params):
   if events == cv2.EVENT_LBUTTONDOWN:
@@ -13,6 +18,9 @@ def mouseClick(events, x, y, flags, params):
       x1, y1 = pos
       if x1 < x < x1+width and y1 < y < y1+height:
         posList.pop(i)
+
+  with open('CarParkPos', 'wb') as f:
+    pickle.dump(posList, f)
 
 while True:
   # cv2.rectangle(img, (50, 192), (157, 240), (255, 0, 255), 2)
